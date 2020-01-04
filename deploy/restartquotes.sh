a7c658065efeaca798243a689fcff6ffb07b7a17
freax <bigbug@freaxnx01.ch>
2020-01-04 20:49:38 +0100

    x

--- deploy/restartquotes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo systemctl daemon-reload
-sudo systemctl restart kestrel-quotes.service
-wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 --tries=inf -O- http://localhost:50001 > /dev/null
-sudo nginx -s reload
--- deploy/runquotes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd quotes
-ASPNETCORE_ENVIRONMENT=Development dotnet Quotes.dll --urls http://0.0.0.0:50001
