9769e638aaf5efd6bbfc02e3b7165155abd4981a
freax <bigbug@freaxnx01.ch>
2020-01-04 21:36:30 +0100

    x

---
+++ AdminUserSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DbHelpers
+{
+    //public class AdminUserSettings
+    //{
+    //    public string RoleName { get; set; }
+    //    public string Email { get; set; }
+    //    public string Pwd { get; set; }
+    //}
+}
---
+++ Controllers/QuotesApiController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Quotes.Data;
+using Quotes.Model;
+
+namespace Quotes.Controllers
+{
+    [Route("Api")]
+    public class QuotesApiController : Controller
+    {
+        private readonly QuotesDbContext _context;
+
+        public QuotesApiController(QuotesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Quote> GetAll()
+        {
+            return _context.Quote.OrderByDescending(q => q.ID).ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetQuote")]
+        public IActionResult GetById(long id)
+        {
+            var item = _context.Quote.FirstOrDefault(t => t.ID == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpGet("random")]
+        public IActionResult GetRandom()
+        {
+            var ids = _context.Quote.Select(q => q.ID).ToList();
+            int randomIndex = new Random().Next(0, ids.Count - 1);
+            return GetById(ids[randomIndex]);
+        }
+    }
+}
