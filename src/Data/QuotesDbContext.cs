8163f749a0b8ea5d544770e80ce328fb5a372322
freax <freax@freaxnx01.ch>
2020-10-22 20:04:46 +0200

    netcoreapp3.1

--- src/Data/QuotesDbContext.cs
+++ src/Data/QuotesDbContext.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Quotes.Model;
 
 namespace Quotes.Data
 {
     public class QuotesDbContext : DbContext
     {
+        public QuotesDbContext()
+        {
+            
+        }
+        
         public QuotesDbContext (DbContextOptions<QuotesDbContext> options)
             : base(options)
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            //=> options.UseSqlite(@"Data Source=data\quote.db");        
+            => options.UseSqlite($"Data Source={new FileInfo(Path.Combine("data", "quote.db")).FullName}");
+        
         public DbSet<Quote> Quote { get; set; }
     }
 }
